d54f30ebb53f5f48f10f74030ac83b99ccc30228
VitalyZibulski <laravel.vitaly.zibulski2017@gmail.com>
2018-06-05 18:01:54 +0300

    add Api request

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, {Component } from 'react';
 import CardList from './CardList';
-import { robots } from './robots';
 import SearchBox from './SearchBox';
 import './App.css';
 
@@ -10,9 +9,15 @@ class App extends Component{
 	constructor(){
 		super()
 		this.state = {
-			robots:robots,
+			robots:[],
 			searchfield:''
 		}
+	}
+
+	componentDidMount(){
+		fetch('https://jsonplaceholder.typicode.com/users')
+		.then(response => response.json())
+		.then(users => this.setState({ robots: users}))
 	}
 
 	onSearchChange = (event) => {
@@ -24,14 +29,18 @@ class App extends Component{
 		const filteredRobots = this.state.robots.filter(robots =>{
 			return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
 		})
-		
-		return (
-		<div className = 'tc'>	
+		if (this.state.robots.length === 0){
+			return <h1>Loading</h1>
+		} else {
+			return (
+			<div className = 'tc'>	
 			<h1 className='f1'>Robots</h1>
 			<SearchBox searchChange={this.onSearchChange} />
 			<CardList robots={filteredRobots}/>
 			</div>
 		);
+		}
+		
 	}
 
 }
